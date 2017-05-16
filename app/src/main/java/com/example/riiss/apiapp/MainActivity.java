8ab8e84e6c50d78a7c1829a6aa8e48835163baca
micker93 <riissanen_@hotmail.com>
2017-05-16 11:14:57 +0200

    update

--- app/src/main/java/com/example/riiss/apiapp/MainActivity.java
+++ app/src/main/java/com/example/riiss/apiapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.riiss.apiapp;
 
 
-import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -28,30 +28,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView=(TextView) findViewById(R.id.textView5);
-
 
         Date today = Calendar.getInstance().getTime();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd, HH:mm");
         String folderName = formatter.format(today);
         textView.setText(folderName);
 
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+        final PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 // TODO: Get info about the selected place.
+
                 city=place.getName().toString();
+
             }
 
             @Override
             public void onError(Status status) {
                 // TODO: Handle the error.
 
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Fel");
+                builder.setMessage("Ett fel inträffades : " + status);
+                builder.setNegativeButton("OK" , null);
+                builder.create();
+                builder.show();
 
             }
         });
@@ -61,24 +69,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void Searchbutton(View v){
 
-
-
         //textviews
         final TextView textView = (TextView) findViewById(R.id.textViewtemp);
         final TextView textView2=(TextView) findViewById(R.id.textViewpressure);
         //imageviews
         ImageView imageView=(ImageView)findViewById(R.id.imageView2);
 
-
-
         String url = String.format("http://api.openweathermap.org/data/2.5/weather?q=" + city
                 +"," + country +"&appid=69585efc9d6d443139ff3fc87c85a87a&units=metric");
-
 
         new GetWeatherCondtition(imageView).execute(url);
         new GetWeatherTask(textView).execute(url);
         new GetPressureTask(textView2).execute(url);
-
 
     }
 
