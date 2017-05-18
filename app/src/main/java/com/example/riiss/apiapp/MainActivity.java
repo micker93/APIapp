2cc78d45e85a70a2d0faaf31d23e2cd076c8144a
micker93 <riissanen_@hotmail.com>
2017-05-18 09:20:41 +0200

    update

--- app/src/main/java/com/example/riiss/apiapp/MainActivity.java
+++ app/src/main/java/com/example/riiss/apiapp/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -63,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
 
     }
 
